c42542a7a8b8c09f7b9531406bc2b3e7505748d8
Kendra Egert <mskendrae@gmail.com>
2016-09-09 10:00:46 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayoneProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String name = Console.ReadLine();
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello "+ name + "." );
+            //Console.WriteLine(name);
+
+            Console.WriteLine("Enter two numbers to be added together");
+            int numberone = int.Parse(Console.ReadLine());
+            int numbertwo = int.Parse(Console.ReadLine());
+            int finalnumber = numbertwo + numberone;
+            Console.WriteLine(finalnumber);
+            
+        }
+    }
+}
